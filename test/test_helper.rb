ece48d2456b72c431658ca033c69276106d79ed9
Jason Roelofs <jameskilton@gmail.com>
2009-02-22 20:48:40 -0500

    1.9 compat

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,14 @@
 $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'rubygems'
+
+begin
+  # 1.9 needs this for test/spec to work
+  gem 'test-unit'
+rescue LoadError
+  # Probably 1.8.x, we're ok
+end
+
 require 'test/spec'
 require 'rbgccxml'
 
